f1e5d0a40c47198252e5ee851fdc96843dd996ad
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-06-17 00:14:15 +0200

    cut-paste

--- dragdrop.clj
+++ dragdrop.clj
@@ -1,8 +1,6 @@
 (ns dragrop.dragdrop
   (:require [clojure.string :as string]))
 
-(def w (slurp "poems.txt"))
-
 (defn trim-nil [s]
   (when s (string/trim s)))
 
@@ -12,7 +10,20 @@
              slurp
              string/split-lines
              (map #(string/split % #";;")))]
-    [(->> vect (map first) (map string/trim))
-     (->> vect (map rest) (map last) (map trim-nil))]))
+    {:poem (->> vect (map first) (map string/trim))
+     :tags (->> vect (map fnext) (map trim-nil))}))
 
-(read-poems "poems.txt")
+(defn cut-out-word [{:keys [poem tags]}]
+  (->> (map (fn [x y] (if y (string/split x (re-pattern y)) [x]))
+            poem tags)
+       (map #(map string/trim %))))
+
+(defn paste-in-word [stub proposal]
+  (->> (map (fn [s p] (if p [(first s) (str " " p " ") (last s)] s))
+            stub proposal)
+       (map string/join)))
+
+(def a (read-poems "poems.txt"))
+(def b (cut-out-word a))
+
+(paste-in-word b (:tags a))
