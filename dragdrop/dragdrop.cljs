11e4c970d1dd25342808062cc9c0ca9f8d6a636f
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2021-07-16 21:50:36 +0200

    inc-dec-verse

--- dragdrop/dragdrop.cljs
+++ dragdrop/dragdrop.cljs
@@ -141,3 +141,34 @@
 
 (defn add-duple-to-matrix [duple matrix]
   (map #(map + %1 %2) matrix (repeat duple)))
+
+(defn inc-verse [a]
+  (let [nof-verses (count (get-in (:verse-lengths lst/config) (butlast a)))
+        nof-poems (count (get (:verse-lengths lst/config) (first a)))
+        nof-categories (count (:verse-lengths lst/config))]
+    (cond
+      (< (inc (get a 2)) nof-verses)
+      (update a 2 inc)
+      (< (inc (get a 1)) nof-poems)
+      (-> a (update 1 inc) (assoc 2 0))
+      (< (inc (get a 0)) nof-categories)
+      (-> a (update 0 inc) (assoc 1 0) (assoc 2 0))
+      :else
+      [0 0 0])))
+
+(defn dec-verse [a]
+  (cond
+    (> (dec (get a 2)) -1)
+    (update a 2 dec)
+    (> (dec (get a 1)) -1)
+    (let [max-verse-idx (dec (count (get-in (:verse-lengths lst/config) [(get a 0) (dec (get a 1))])))]
+      (-> a (update 1 dec) (assoc 2 max-verse-idx)))
+    (> (dec (get a 0)) -1)
+    (let [max-poems-idx (dec (count (get (:verse-lengths lst/config) (dec (get a 0)))))
+          max-verse-idx (dec (count (get-in (:verse-lengths lst/config) [(dec (get a 0)) max-poems-idx])))]
+      (-> a (update 0 dec) (assoc 1 max-poems-idx) (assoc 2 max-verse-idx)))
+    :else
+    (let [max-categories-idx (dec (count (:verse-lengths lst/config)))
+          max-poems-idx (dec (count (get (:verse-lengths lst/config) max-categories-idx)))
+          max-verse-idx (dec (count (get-in (:verse-lengths lst/config) [max-categories-idx max-poems-idx])))]
+      [max-categories-idx max-poems-idx max-verse-idx])))
